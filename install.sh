ce382611f80962dc1c30bbdc361f1cc4b04f1b7c
errantspark <errantspark@gmail.com>
2014-10-24 19:20:34 +0000

    slit out linking

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #This is the unattended install script
 apt-get -y install nodejs zsh ack-grep
-ln -s /usr/bin/nodejs /usr/bin/node
-eval ln .zshrc ~$USERN
 
 #this will include setting up zsh dotfiles and setting up vim, cloning fresh plugins
 #once i'm done
