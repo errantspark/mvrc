878496147869f9f4c226e853c0ff38a7e3162984
errantspark <errantspark@gmail.com>
2019-05-07 03:21:29 -0700

    testable?

--- system_setup.sh
+++ system_setup.sh
@@ -66,43 +66,14 @@ yay -S --needed --noconfirm nvm
 nvm install node
 
 ######### GARBAGE LINKING
-#clean things, this is only useful for re-linking stuff in edge cases
-#TODO i should really write a more robust update tool
-rm $UD/.zshrc
-rm $UD/.zprofile
-rm $UD/.vimrc
-rm $UD/.bash_aliases
-rm $UD/.screenrc
-rm $UD/.vim/colors/noctu.vim
-rm $UD/.vim/colors/orchid.vim
-rm $UD/.vim/.editorconfig
-#rm $UD/.gitconfig
-#set up vim swap and backup folders
-mkdir $UD/.vimswap 
-mkdir $UD/.vimbackup 
-mkdir $UD/.vim
-mkdir $UD/.vim/colors
-#link up rc files
-cwd=$(pwd)
-ln -s $cwd/misc $UD/.misc
-ln -s $cwd/rc/.zshrc $UD
-ln -s $cwd/rc/.zprofile $UD
-ln -s $cwd/rc/.vimrc $UD
-ln -s $cwd/rc/.bash_aliases $UD
-ln -s $cwd/rc/.screenrc $UD
-#link up colorschemes
-ln -s $cwd/vim/noctu.vim $UD/.vim/colors/
-ln -s $cwd/vim/orchid.vim $UD/.vim/colors/
-#link up editorconfig for jsbeautify etc
-ln -s $cwd/vim/.editorconfig $UD/.vim/
-
-############################
+# ./link.sh
 
 #copy .gitinfo so userland setup script can populate it with email/password
 cp rc/.gitconfig $UD
 
+###### NOT USING THIS SHIT ANYMORE DELETE
 #clone vundle
-git clone https://github.com/gmarik/vundle.git $UD/.vim/bundle/vundle
+# git clone https://github.com/gmarik/vundle.git $UD/.vim/bundle/vundle
 
 #install vundle plugins
-vim +PluginInstall +qall
+# vim +PluginInstall +qall
