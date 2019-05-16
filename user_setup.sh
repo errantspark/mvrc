a985ffda35e725e9c4060d306bb626c9744be863
errantspark <errantspark@gmail.com>
2019-05-16 17:06:27 +0000

    fixed newlines

--- user_setup.sh
+++ user_setup.sh
@@ -4,16 +4,18 @@
 #shouldn't need this anymore I think?
 #sudo chown -R $USER: ~
 
-echo "Setup SSH and Git\n\nssh..."
+echo -e "Setup SSH and Git\n\nssh..."
 #dunno what this does, it's from the github tut
 read -p "email > " email
 ssh-keygen -t rsa -C $email
 #ssh-agent -s
 #ssh-add ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
-read -p "copy paste this or use\n\ncat ~/.ssh/id_rsa.pub | xclip\n\npress any key to continue"
+echo -e "copy paste this or use\n\ncat ~/.ssh/id_rsa.pub | xclip\n\npress any key to continue"
+read
 
-read -p "git...\n\ngithub username> " gname
+echo -e "git...\n\ngithub username> "
+read gname
 
 git config --global user.name $gname
 git config --global user.email $email
